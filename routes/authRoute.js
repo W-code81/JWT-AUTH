c7af4679fe3b356759cfe5acfaa9ee94431c639d
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-05-29 18:41:56 +0100

    feat: login route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const authRouter = express.Router();
-const signup  = require('../controllers/authControllers');
+const {signup, login}  = require('../controllers/authControllers');
 
 
 authRouter.post('/signup', signup);
-
+authRouter.post('/login', login);
 module.exports = authRouter;
