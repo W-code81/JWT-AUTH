90839d8781cc228847fb52aed8d6061cf938e5cd
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-05-31 21:57:58 +0100

    feat: protected all users route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const authRouter = express.Router();
 const { signup, login, getUsers, forgotPassword, getResetPassword, changePassword } = require('../controllers/authControllers');
+const protect = require('../middleware/authMiddleware')
 
 
 authRouter.post('/signup', signup);
 authRouter.post('/login', login);
-authRouter.get('/all-users', getUsers)
+authRouter.get('/all-users', protect , getUsers)
 authRouter.post('/forgot-password', forgotPassword)
 authRouter.route('/reset-password/:token')
     .get(getResetPassword)
