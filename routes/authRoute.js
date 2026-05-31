a34b884753cde5b227f91c136af30e21900837c5
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-05-31 21:23:40 +0100

    feat: added controllers to routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const authRouter = express.Router();
-const {signup, login}  = require('../controllers/authControllers');
+const { signup, login, getUsers, forgotPassword, getResetPassword, changePassword } = require('../controllers/authControllers');
 
 
 authRouter.post('/signup', signup);
 authRouter.post('/login', login);
+authRouter.get('/all-users', getUsers)
+authRouter.post('/forgot-password', forgotPassword)
+authRouter.route('/reset-password/:token')
+    .get(getResetPassword)
+    .post(changePassword)
 module.exports = authRouter;
