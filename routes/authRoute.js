838b90a3532e7564e8a97d60c1698c143222cc51
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-06-01 10:36:10 +0100

    feat: logout route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const authRouter = express.Router();
-const { signup, login, getUsers, forgotPassword, getResetPassword, changePassword } = require('../controllers/authControllers');
+const { signup, login, getUsers, forgotPassword, getResetPassword, changePassword, logOut } = require('../controllers/authControllers');
 const protect = require('../middleware/authMiddleware')
 
 
 authRouter.post('/signup', signup);
 authRouter.post('/login', login);
+authRouter.post('/logOut', logOut)
 authRouter.get('/all-users', protect , getUsers)
 authRouter.post('/forgot-password', forgotPassword)
 authRouter.route('/reset-password/:token')
