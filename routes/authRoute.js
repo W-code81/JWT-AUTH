9d4b0f3959e2db6f0bf9eb2ffc84d8679e5a73e4
oluwadamilolaaderogba@gmail.com <oluwadamilolaaderogba@gmail.com>
2026-06-03 13:48:06 +0100

    feat:added auth to all-users route       added new routes and controllers

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const authRouter = express.Router();
-const { signup, login, getUsers, forgotPassword, getResetPassword, changePassword, logOut } = require('../controllers/authControllers');
-const protect = require('../middleware/authMiddleware')
+const { signup, login, getUsers, forgotPassword, getResetPassword, changePassword, logOut ,refreshAccessToken, getMe } = require('../controllers/authControllers');
+const {protect, authorize} = require('../middleware/authMiddleware')
 
 
 authRouter.post('/signup', signup);
 authRouter.post('/login', login);
 authRouter.post('/logOut', logOut)
-authRouter.get('/all-users', protect , getUsers)
+authRouter.get('/all-users', protect , authorize('admin'), getUsers) //protect checked if user is loged in and authorize validates role
+authRouter.post('/refresh', refreshAccessToken);
+authRouter.get('/me', protect, getMe);    
 authRouter.post('/forgot-password', forgotPassword)
 authRouter.route('/reset-password/:token')
     .get(getResetPassword)
